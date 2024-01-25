3784af84234a52b3aceea0a397f7d9c59035ec38
KislyukAnna <kis-annet@mail.ru>
2024-01-25 17:54:15 +0800

    add seminar1:hw2

---
+++ seminar1/hw2/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 8;
+int b = 12;
+int c = 3;
+
+int result = a;
+
+if (b > result)
+{
+result = b;
+}
+if (c > result)
+{
+result = c;
+}
+
+//return (result);
+System.Console.WriteLine(result);
