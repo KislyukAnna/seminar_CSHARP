827f3e98c7f7e19d2e41ace9f9bde1461f03fe6e
KislyukAnna <kis-annet@mail.ru>
2024-01-29 19:24:47 +0800

    add seminar2:homework_4

---
+++ seminar2/homework_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+System.Console.Write("Введите натуральное число:");
+int N = int.Parse(Console.ReadLine());
+
+string str_N = N.ToString();
+
+System.Console.WriteLine(str_N);
+
+for (int i = 0; i < str_N.Length; i++)
+{
+    System.Console.Write($"{str_N[i]}");
+
+    if (i != str_N.Length - 1)
+    {
+        System.Console.Write(",");
+    }
+}
