f372cf412041deffef58e133963188d8373f1aa0
KislyukAnna <kis-annet@mail.ru>
2024-01-29 18:56:46 +0800

    add seminar2:homework_3

---
+++ seminar2/homework_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.Write("Введите число из отрезка [10, 99]:");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 10 & num <=99)
+{
+    int first_num=num/10;
+    int second_num=num%10;
+
+    if (first_num>=second_num)
+    {
+        System.Console.WriteLine(first_num);
+    }
+    else
+    {
+        System.Console.WriteLine(second_num);
+    }
+}
+else
+{
+    System.Console.WriteLine("ты точно хотел это ввести?");
+}
