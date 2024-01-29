ddcd18bb5350c16095ae62287e5078c7e0cd8aa1
KislyukAnna <kis-annet@mail.ru>
2024-01-29 15:59:10 +0800

    add seminar2:task2

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает на вход трехзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре
+
+int a = 487;
+
+//double result = Math.Pow((a % 100) / 10, (a % 10));
+int second = (a % 100) / 10;
+int therd = a % 10;
+int result = 1;
+
+for (int i = 1; i <= therd; i++)
+{
+    result = result*second;
+}
+
+System.Console.WriteLine(result);
