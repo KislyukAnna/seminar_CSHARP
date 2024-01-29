90badd74b552898a9cb961f909d742e716f705f1
KislyukAnna <kis-annet@mail.ru>
2024-01-29 18:38:13 +0800

    add seminar2:homework_2

---
+++ seminar2/homework_2/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Введите координату Х:");
+int x = int.Parse(Console.ReadLine());
+
+System.Console.Write("Введите координату Y:");
+int y = int.Parse(Console.ReadLine());
+
+//System.Console.WriteLine($"({x},{y})");
+
+if (x == 0 | y == 0)
+{
+    System.Console.WriteLine("точка лежит на оси координат");
+}
+else if (x > 0 & y > 0)
+{
+    System.Console.WriteLine("1");
+}
+else if (x < 0 & y > 0)
+{
+    System.Console.WriteLine("2");
+}
+else if (x < 0 & y < 0)
+{
+    System.Console.WriteLine("3");
+}
+else if (x > 0 & y < 0)
+{
+    System.Console.WriteLine("4");
+}
