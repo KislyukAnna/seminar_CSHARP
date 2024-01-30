89c83e54ec1e9278e2f963f540146e5693a9eb12
KislyukAnna <kis-annet@mail.ru>
2024-01-30 19:08:03 +0800

    add seminar2:homework4_1

---
+++ seminar2/homework4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Вывод цифр числа через запятую
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{   
+    string str_N = (N%10).ToString();
+
+    N /=10;
+
+    while (N > 0)
+    {
+        int number = N % 10;
+        
+        str_N = number.ToString() + "," + str_N;
+
+        N /= 10;
+    }
+    System.Console.WriteLine(str_N);
+}
