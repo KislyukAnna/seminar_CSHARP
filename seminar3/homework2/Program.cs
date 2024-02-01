38b186b9d5b6a8cdf7ffcb21ef1bbd6e2e2bc124
KislyukAnna <kis-annet@mail.ru>
2024-02-01 18:25:08 +0800

    add seminar3:homework2

---
+++ seminar3/homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        //Напишите свое решение здесь        
+      int count = 0;
+
+       for (int i = 0; i < numbers.Length; i++)
+       {
+            if (numbers[i] % 2 == 0)
+            {
+                count += 1;
+            }
+        }
+        return count;
+
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+        int resultCount = CountEvenItems(array);
+        Console.WriteLine(resultCount);
+ 
+    }
+}
