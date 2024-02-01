a4b0b40eed50224769f6bb7a8f5856e7025e8297
KislyukAnna <kis-annet@mail.ru>
2024-02-01 18:25:34 +0800

    add seminar3:homework1

---
+++ seminar3/homework1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{
+    // Подсчет количества элементов массива, попадающих в заданный диапазон
+    // numbers - массив, в котором ведется подсчет
+    // minRange - минимальная граница диапазона
+    // maxRange - максимальная граница диапазона
+    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+    {
+        //Введите сюда свое решение
+
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] >= minRange & numbers[i] <= maxRange)
+            {
+                count += 1;
+            }
+        }
+        return count;
+
+    }
+
+    public static void PrintResult(int[] array)
+    {
+
+        //Введите сюда свое решение
+
+        int resultCount = CountItemsRange(array, 10, 90);
+        Console.WriteLine(resultCount);
+
+    }
+
+}
+
