91ff506a1e9b404a03eefce6d9b17b81897b8720
KislyukAnna <kis-annet@mail.ru>
2024-02-06 14:47:19 +0800

    add seminar4:homework3

---
+++ seminar4/homework3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+
+// создание рандомного массива с ручным вводом размера массива
+int[] GetArraySizeFromConsole()
+{
+    Console.Write("Введи размер массива : ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 101);
+    }
+    return array;
+}
+
+// вывод массива
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    foreach (int number in array)
+    {
+        Console.Write(number + " ");
+    }
+    Console.WriteLine("]");
+}
+
+//функция "зеркало" массива
+int[] MirroringArray(int[] array)
+{
+    int temp;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    return array;
+}
+
+
+
+Console.Clear();
+int[] array = GetArraySizeFromConsole();
+PrintArray(array);
+MirroringArray(array);
+PrintArray(array);
