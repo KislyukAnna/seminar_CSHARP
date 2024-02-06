74b0e19147d9c8fd20c252f322bda5fa074e1f2e
KislyukAnna <kis-annet@mail.ru>
2024-02-06 13:32:14 +0800

    add seminar4:homework1

---
+++ seminar4/homework1/Program.cs
@@ -0,0 +1,68 @@
+﻿//является ли числом
+bool IsNum(string input)
+{
+    int output;
+    bool result = int.TryParse(input, out output);
+    return result;
+}
+
+
+//Сумма цифр числа 
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+
+        sum += digit;
+
+        number /= 10;
+    }
+    return sum;
+}
+
+//функция определения нечетности числа
+bool IsEven(int number)
+{
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+while (true)
+{
+    System.Console.Write("Введите целое число или q для выхода: ");
+    string input = Console.ReadLine()!;
+
+    if (input == "q")
+    {
+        System.Console.WriteLine("Exit");
+        break;
+    }
+    else
+    {
+        if (IsNum(input))
+        {
+            int number = int.Parse(input);
+            if (IsEven(SumOfDigits(number)))
+            {
+                System.Console.WriteLine("Сумма цифр числа четная. Exit");
+                break;
+            }
+
+        }
+        else
+        {
+            System.Console.WriteLine("Ошибка ввода. Попробуй еще раз.");
+        }
+    }
+
+}
+
+
